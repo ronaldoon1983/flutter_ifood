e3f2dbffd93e8a48a231863e15ad87aaeb5960c3
Ronaldo <ronaldoon@gmail.com>
2020-10-01 14:50:57 -0300

    Inicio1

--- lib/containers/widgets/location.dart
+++ lib/containers/widgets/location.dart
@@ -4,6 +4,7 @@ class Location extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      
       color: Colors.white,
       child: Padding(
         padding: EdgeInsets.symmetric(
@@ -13,23 +14,32 @@ class Location extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            
             Text(
-              'ENTREGAR EM',
-              style: TextStyle(
-                color: Colors.grey,
-                fontWeight: FontWeight.w700,
-                fontSize: 16,
-              ),
+            'ENTREGAR EM',
+            style: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w700,
+            fontSize: 16,
+            ),
+            
             ),
             Row(
               children: <Widget>[
-                Text(
-                  'Stn, 716',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
+                TextFormField(
+  decoration: const InputDecoration(
+    icon: Icon(Icons.person),
+    hintText: 'What do people call you?',
+    labelText: 'Name *',
+  ),
+  onSaved: (String value) {
+    // This optional block of code can be used to run
+    // code when the user saves the form.
+  },
+  validator: (String value) {
+    return value.contains('@') ? 'Do not use the @ char.' : null;
+  },
+),
                 Icon(
                   Icons.keyboard_arrow_down,
                   color: Colors.red,
